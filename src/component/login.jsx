38b8676096a5f442e0b930584231747c067b0a90
Gracesvg <134380789+Gracesvg@users.noreply.github.com>
2026-03-30 22:23:43 +0300

    set  up a a login form with useState and error handling to handle sign-ins.

--- src/component/login.jsx
+++ src/component/login.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import {useState} from 'react'
 
-function login() {
+export default function login () {
+    const [formData, setFormData] = useState({email : "", password : ""})
+    const [error, setError] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        if (!formData.email || !formData.password) {
+            setError("Both fields are required!")
+            return
+        }
+        setError("")
+        console.log("Logging in with:", formData)
+    }
+
   return (
-    <div>login</div>
+    <div className='max-w-md mx-auto mt-10 p-6 border rounded shadow'>
+        <h2 className='text-2xl font-bold mb-4'>login</h2>
+            <form onSubmit= {handleSubmit} className='space-y-4'>
+                <div>
+                    <label>Email</label>
+                    <input type = "email" className='w-full border p-2 rounded' value = {formData.email} onChange={(e) => setFormData({...formData, email: e.target.value})}/>
+                </div>
+                {error && <p className='text-red-500 text-sm'>{error}</p>}
+                <button type='submit' className='w-full'>Sign in</button>
+            </form>
+    </div>
   )
 }
-
-export default login
